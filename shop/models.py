2d00474c816ba035f0d371824d40ec286adf499a
AmiliaWolfram <aminavolkova15@gmail.com>
2023-03-08 19:33:28 +0600

    third hw

--- shop/models.py
+++ shop/models.py
@@ -12,7 +12,7 @@ class Item(models.Model):
 class Purchase(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField(default=18)
-    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name='purchases')
 
     def __str__(self):
         return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,1 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.urls import reverse
+
+from .models import Item, Purchase
+
+
+def list_item(request):
+    last_item_list = Item.objects.all()
+    context = {
+        'items': last_item_list,
+    }
+    return render(request, 'list_item.html', context)
+
+
+def detail_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    return render(request, 'detail_item.html', {'item': item})
